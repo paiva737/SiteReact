e52a90659e8664add42841a3459e5cafbb0125b6
paiva737 <rafaelpaiva636@gmail.com>
2024-09-30 18:05:23 -0300

    adicionando aifilheart rodape

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './rodape.css'
-
+import { AiFillHeart } from 'react-icons/ai';
 const Rodape = () => {
     return (<footer className="footer">
      
        <section>
         <p>
-            Desenvolvido por Rafael Paiva.
+        <AiFillHeart/>  Desenvolvido por Rafael Paiva.
         </p>
        </section>
     </footer>)
